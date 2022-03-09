85cc33aadbef03bac8f0d1bef5f41f9ee895c02e
Kim Stocker <kimbstocker@gmail.com>
2022-03-09 17:40:36 +1100

    add dynamic flash messages

--- src/buysell2game/app/helpers/application_helper.rb
+++ src/buysell2game/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
-end
+    def select_alert_subclass(input)
+      case input 
+      when 'notice'
+        return 'success'
+      when 'alert'
+        return 'warning'
+      when 'error'
+        return 'danger'
+      else
+        return 'info'
+      end 
+    end 
+  end
+  
