05c510c2ccf69941acea5a425822153bbe2e366f
Kim Stocker <kimbstocker@gmail.com>
2022-03-11 09:48:57 +1100

    change new to brandnew in enum

--- src/buysell2game/app/models/listing.rb
+++ src/buysell2game/app/models/listing.rb
@@ -2,5 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  enum condition: {new: 1, like_new: 2, great: 3, good: 4, fair: 5}
+  enum condition: {brand_new: 1, like_new: 2, great: 3, good: 4, fair: 5}
 end
