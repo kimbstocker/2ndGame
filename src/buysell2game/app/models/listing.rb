00108cf3e0147db3e9f60e0432234fce9550fd51
Kim Stocker <kimbstocker@gmail.com>
2022-03-20 14:52:20 +1100

    add obscenity gem

--- src/buysell2game/app/models/listing.rb
+++ src/buysell2game/app/models/listing.rb
@@ -14,7 +14,29 @@ class Listing < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :users, through: :favourite
 
-  #data validations, even the form include :require => true, this create an extra layer to ensure data received is valid
+  # Data validations, even the form include :require => true, this create an extra layer to ensure data received is valid
   validates :listing_name, :condition, :price, :description, :category_id, :shipping, presence: true
   validates :listing_name, length: { minimum: 2 }
+
+  # Data sanitsations
+  before_save :remove_whitespace
+  before_save :remove_covid
+  before_validation :price_round
+
+  private 
+  
+  def remove_whitespace 
+    self.title = self.title.strip
+    self.description = self.description.strip
+  end
+  
+  def remove_covid 
+    self.title = self.title.gsub(/covid/i, "pfizer")
+    self.description = self.title.gsub(/covid/i, "pfizer")
+  end
+  
+  def price_round 
+    self.price = (self.attributes_before_type_cast["price"]).round
+  end 
+  
 end
